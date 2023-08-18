0154c1f73d1dfeb3b13ace7505f984d408596a17
cherry-san <123456789+cheryl.s.kouadio@gmail.com>
2023-08-18 16:33:59 +0200

    Write php file for making appointment

---
+++ envoi.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_POST['envoyer'])) {
+  $nom = $_POST['nom'];
+  $email = $_POST['email'];
+  $comment = $_POST['comment'];
+  $service = $_POST['service'];
+  $date = $_POST['date'];
+  $periode = $_POST['periode'];
+
+
+
+  // Destinataire de l'e-mail
+  $destinataire = 'cabinet.beaudelaie.conseil@gmail.com';
+
+  // Sujet de l'e-mail
+  $sujet = 'Prise de rendez-vous pour une affaire '.$service;
+
+  // Construire le corps du message
+  $contenu = "Nom : $nom\n";
+  $contenu .= "Email : $email\n\n";
+  $contenu .= "Message :\n$comment\n";
+
+  // En-têtes de l'e-mail
+  $headers = "From: $nom <$email>\r\n";
+  $headers .= "Reply-To: $email\r\n";
+
+  // Envoyer l'e-mail
+  if(mail($destinataire, $sujet, $contenu, $headers) && mail($destinataire2, $sujet, $contenu, $headers)) {
+    echo "<script type='text/javascript'>alert('Votre message a été envoyé avec succès');</script>" ;
+  } else {
+    echo "<script type='text/javascript'>Une erreur s'est produite lors de l'envoi du message;</script>";
+  }
+}
+?>
